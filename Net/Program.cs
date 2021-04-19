5ce9e2eae81822e50ecf26eb187d68351c8c59d5
LetRuin <crot9000@yandex.ru>
2021-04-19 20:42:04 +0800

    создан соседний проект интернет

---
+++ Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
