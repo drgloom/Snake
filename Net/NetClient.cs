87b1ea8b017f633df8afe96ab5fd63a8cd0acee2
LetRuin <crot9000@yandex.ru>
2021-04-20 21:16:09 +0800

    Реализован классы связи через интернет

--- Net/NetClient.cs
+++ Net/NetClient.cs
@@ -87,5 +87,54 @@ namespace Snake
             if (client != null)
                 client.Close();
         }
+        
+        static public  void SendMessage(NetworkStream stream)
+        {
+            Console.WriteLine("Введите сообщение: ");
+             
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        // получение сообщений
+        static public void ReceiveMessage(NetworkStream stream,TcpClient client)
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+ 
+                    string message = builder.ToString();
+                    Console.WriteLine(message);//вывод сообщения
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect(stream,client);
+                }
+            }
+        }
+ 
+        static public void Disconnect(NetworkStream stream,TcpClient client)
+        {
+            if(stream!=null)
+                stream.Close();//отключение потока
+            if(client!=null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
     }
 }
--- Net/Program.cs
+++ Net/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Threading;
+using System.Net.Sockets;
+using System.Text;
  
 namespace Snake
 {
+    
     class Program
     {
         static NetServer server; // сервер
         static Thread listenThread; // потока для прослушивания
-        static void Main(string[] args)
+        
+        static string userName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+        
+        static private void Server()
         {
+            Console.WriteLine("проверка");
             try
             {
                 server = new NetServer();
@@ -20,6 +31,55 @@ namespace Snake
                 server.Disconnect();
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        static private void Client()
+        {
+            Console.Write("Введите свое имя: ");
+            userName = Console.ReadLine();
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+ 
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+ 
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart((() => NetClient.ReceiveMessage(stream,client))));
+                receiveThread.Start(); //старт потока
+                Console.WriteLine("Добро пожаловать, {0}", userName);
+                NetClient.SendMessage(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                NetClient.Disconnect(stream,client);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("1 сервер 2 клиент");
+            num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Server();
+                    break;
+                case 2:
+                    Client();
+                    break;
+                default:
+                    Console.WriteLine("проверка");
+                    break;
+            }
+            
         }
     }
 }
